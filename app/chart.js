4e55f85e5fa81aad0a371b348815c4980db8ab22
Ehsan-c137 <ehsanmousavi1.79@gmail.com>
2022-04-05 17:27:10 +0430

    change chartJsFuncitons

--- app/chart.js
+++ app/chart.js
@@ -8,17 +8,28 @@ function getChartDays(data) {
       });
       chartJsWeek.push(time);
    }
+
    myChart.update();
 }
 
 let humidityData = [];
+function checkData() {
+   if (humidityData.length > 0) {
+      humidityData = [];
+      myChart.data.datasets[0].data = [];
+      myChart.update();
+   }
+}
 function showDailyHumidity(d) {
-   for (let i = 0; i <= 7; i++) {
+   checkData();
+   for (let i = 0; i < 7; i++) {
       humidityData.push(d.daily[i].humidity);
    }
-   // chart js stuff
+   myChart.data.datasets[0].data = humidityData;
    myChart.update();
+   console.log(humidityData);
 }
+
 const ctx = document.getElementById("myChart").getContext("2d");
 const myChart = new Chart(ctx, {
    type: "bar",
@@ -27,35 +38,12 @@ const myChart = new Chart(ctx, {
       datasets: [
          {
             label: "humidity",
-            data: humidityData,
+            data: [],
             backgroundColor: ["rgba(54, 162, 235, 0.2)"],
             borderColor: ["rgba(54, 162, 235, 1)"],
             borderWidth: 1,
          },
       ],
-      // datasets: [
-      //    {
-      //       label: "# of Votes",
-      //       data: [12, 19, 3, 5, 2, 3],
-      //       backgroundColor: [
-      //          "rgba(255, 99, 132, 0.2)",
-      //          "rgba(54, 162, 235, 0.2)",
-      //          "rgba(255, 206, 86, 0.2)",
-      //          "rgba(75, 192, 192, 0.2)",
-      //          "rgba(153, 102, 255, 0.2)",
-      //          "rgba(255, 159, 64, 0.2)",
-      //       ],
-      //       borderColor: [
-      //          "rgba(255, 99, 132, 1)",
-      //          "rgba(54, 162, 235, 1)",
-      //          "rgba(255, 206, 86, 1)",
-      //          "rgba(75, 192, 192, 1)",
-      //          "rgba(153, 102, 255, 1)",
-      //          "rgba(255, 159, 64, 1)",
-      //       ],
-      //       borderWidth: 1,
-      //    },
-      // ],
    },
    options: {
       scales: {
@@ -66,3 +54,12 @@ const myChart = new Chart(ctx, {
       responsive: true,
    },
 });
+
+// function addData(chart, data) {
+//    // chart.data.labels.push(label);
+
+//    chart.data.datasets.forEach((dataset) => {
+//       dataset.data.push(...data);
+//    });
+//    chart.update();
+// }
